6ac973aaa6bf2248feccb374010f28db69c16c2e
youtanimstar <animdeep2019@gmail.com>
2024-01-07 20:20:34 +0530

    Tailwind css added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ const App = () => {
   return (
     <>
       <h1>WELCOME TO HACKERSPACE</h1>
+      <h1 className="text-3xl font-bold underline bg-red-600">
+      Hello world!
+    </h1>
     </>
   );
 };
