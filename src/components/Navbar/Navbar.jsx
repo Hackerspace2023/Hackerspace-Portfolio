72b53fdabab6b6505ab5c4eac5ef49d0e7bfad2c
youtanimstar <animdeep2019@gmail.com>
2024-01-20 15:46:30 +0530

    button added and navbar updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,43 @@
-import React from 'react'
-import "../../css/navbar.css"
+import React, { useEffect, useState } from "react";
+import "../../css/navbar.css";
+import Button from "../Button/Button";
+import { useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  const location = useLocation();
+  const { pathname } = location;
+  
+  useEffect(() => {
+    
+    if (path) {
+      setPath(pathname.replace(/^\/|\/$/g, ""));
+      
+    } else {
+      setPath("home");
+    }
+  }, [pathname]);
+  const [path, setPath] = useState(pathname.replace(/^\/|\/$/g, ""));
+
   return (
     <>
       <nav className="navbar">
         <div className="navItem">
           <div className="sectionNumber">//01.</div>
-          <div className="sectionTitle">{`<Home/>`}</div>
+          <div className="sectionTitle">{`<${
+            pathname.replace(/^\/|\/$/g, "")
+              ? pathname.replace(/^\/|\/$/g, "")
+              : "home"
+          }/>`}</div>
         </div>
         <div className="navItem">
           <div className="navLogo">HS</div>
         </div>
-        <div className="navItem"><button>Join US</button></div>
+        <div className="navItem">
+          <Button text="Join US" to={"/contact"} />
+        </div>
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
