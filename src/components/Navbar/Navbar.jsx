195a6ffda0470c3a3c5c1c549ecf7540c853fe45
youtanimstar <animdeep2019@gmail.com>
2024-01-20 09:00:58 +0530

    navbar,container css added and section modified

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import "../../css/navbar.css"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+      <nav className="navbar">
+        <div className="navItem">
+          <div className="sectionNumber">//01.</div>
+          <div className="sectionTitle">{`<Home/>`}</div>
+        </div>
+        <div className="navItem">
+          <div className="navLogo">HS</div>
+        </div>
+        <div className="navItem"><button>Join US</button></div>
+      </nav>
+    </>
   )
 }
 
