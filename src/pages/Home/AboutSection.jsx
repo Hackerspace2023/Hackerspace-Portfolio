2beda43b6be6743253f736178ecbcebe355f74ea
sayansanpui <sayansanpuicoding@gmail.com>
2024-01-26 17:17:54 +0530

    Sayan Uplode

---
+++ src/pages/Home/AboutSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../../css/AboutSection.css"
+
+const AboutSection = () => {
+    return (
+        <section className="about">
+            <div className='titleContainer'>
+                <span className='tagAbout tagAbout1'>{`<h2>`}</span>
+                <h1 className='title'>About US</h1>
+                <span className='tagAbout tagAbout2'>{`</h2>`}</span>
+            </div>
+        </section>
+    )
+}
+
+export default AboutSection
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import HomeSection from './HomeSection'
+import AboutSection from './AboutSection'
 
 
 const Home = () => {
   return (
     <>
       <HomeSection/>
+      <AboutSection/>
     </>
   )
 }
