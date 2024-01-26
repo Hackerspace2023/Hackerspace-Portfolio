1ed378a19a23a83e5cc7763d47078789beae785a
sayansanpui <sayansanpuicoding@gmail.com>
2024-01-26 17:34:35 +0530

    Deep Uplode

--- src/pages/Home/AboutSection.jsx
+++ src/pages/Home/AboutSection.jsx
@@ -7,6 +7,7 @@ const AboutSection = () => {
             <div className='titleContainer'>
                 <span className='tagAbout tagAbout1'>{`<h2>`}</span>
                 <h1 className='title'>About US</h1>
+                <h1>Sanpui</h1>
                 <span className='tagAbout tagAbout2'>{`</h2>`}</span>
             </div>
         </section>
