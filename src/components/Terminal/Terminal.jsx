4c66998805801cad630686388a7fe4fa0c47dd5b
youtanimstar <animdeep2019@gmail.com>
2024-03-03 17:56:53 +0530

    terminal added

---
+++ src/components/Terminal/Terminal.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../../css/terminal.css";
+import { IoSend } from "react-icons/io5";
+import { nav } from "../../data";
+
+const Terminal = () => {
+  const navigate = useNavigate();
+  const [terminal, setTerminal] = useState("");
+  const [show, setShow] = useState(false);
+  const data = Object.keys(nav);
+  const navData = [...data.filter((item) => item !== "error")];
+  console.log(navData);
+  const handleForm = (e) => {
+    e.preventDefault();
+    navigate(`${terminal}`);
+    setTerminal("");
+    setShow(false);
+  };
+
+  const handleChanges = (e) => {
+    if (e.target.value === "/") {
+      setShow(true);
+    } else if (e.target.value === "") {
+      setShow(false);
+    }
+    setTerminal(e.target.value);
+  };
+  const handleClick = (item) => {
+    navigate(`${item}`);
+    setTerminal("");
+    setShow(false);
+  };
+  return (
+    <>
+      <form className="terminal" onSubmit={handleForm} >
+        <input
+          type="text"
+          onChange={handleChanges}
+          className="input"
+          value={terminal}
+          placeholder="Tip: press / for more info"
+        />
+        <button type="submit" className="submit">
+          <IoSend />
+        </button>
+        {show && (
+          <div className="moreInfo">
+            {navData.map((item, index) => {
+              return (
+                <div
+                  className="moreInfoItem"
+                  key={index}
+                  onClick={()=>handleClick(item)}
+                >
+                  /{item}
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </form>
+    </>
+  );
+};
+
+export default Terminal;
--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Divider from "./Divider";
 import Footer from "./Footer/Footer";
 import Navbar from "./Navbar/Navbar";
 import ImageBorder from "./ImageBorder/ImageBorder";
+import Terminal from "./Terminal/Terminal";
 
-export { Navbar, Footer, Button, ImageBorder, Divider };
+export { Navbar, Footer, Button, ImageBorder, Divider , Terminal};
