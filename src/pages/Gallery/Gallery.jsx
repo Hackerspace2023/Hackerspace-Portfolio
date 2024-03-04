76d46c1473c630c1471b0267121aad3417f9155e
sayansanpui <sayansanpuicoding@gmail.com>
2024-03-04 12:27:28 +0530

    update

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -1,8 +1,49 @@
+import React from "react";
+import "../../css/gallery.css";
+import Swiper from "swiper";
+
 const Gallery = () => {
+  const data = [
+    {
+      profilePic: "profileImg.jpg",
+      name: "Lorem Ipsam",
+      // role: "DESIGN AND DEVELOPMET",
+    },
+    {
+      profilePic: "profileImg2.jpeg",
+      name: "Lorem Ipsam",
+      // role: "DESIGN AND DEVELOPMET",
+    },
+  ];
   return (
-    <div>
-      <h1>Gallery</h1>
-    </div>
+    <section className="gallery">
+      <div className="galleryAbout">
+        <div className="titleContainer">
+          <span className="tagAbout tagAbout1">{`<h2>`}</span>
+          <h1 className="title">Gallery</h1>
+          <span className="tagAbout tagAbout2">{`</h2>`}</span>
+        </div>
+        <div className="galleryContent">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has beenLorem Ipsum is simply dummy text of the
+          printing and typesetting industry. Lorem Ipsum has been the industry's
+          standard dummy text Lorem Ipsum is simply dummy text of the printing
+          and typesetting industry. Lorem Ipsum has been
+        </div>
+      </div>
+      <div className="galleryAbout">
+        <div className="titleContainer">
+          <span className="tagAbout tagAbout1">{`<h2>`}</span>
+          <h1 className="title">Our Members</h1>
+          <span className="tagAbout tagAbout2">{`</h2>`}</span>
+        </div>
+      </div>
+      <div className="members">
+        
+      </div>
+    </section>
   );
 };
 
