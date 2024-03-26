4d036ee8ec56c3bf32a57542f701d86b2d62e2e7
Sayan Sanpui <sayansanpuicoding@gmail.com>
2024-03-26 12:24:33 +0530

    Project Section update design

--- src/components/Category/ProjectCategory.jsx
+++ src/components/Category/ProjectCategory.jsx
@@ -1,9 +1,19 @@
 import React, { Fragment, useState } from "react";
 import "../../css/eventsCategory.css";
+import CostumeHeading from "../Heading/CostumeHeading";
 
 const ProjectCategory = ({ data, category, domain, renderComponet }) => {
   const uniqueCategory = [...new Set(data.map((item) => item[category]))];
+  const uniqueDomain = [...new Set(data.map((item) => item[domain]))];
   const [selectedId, setSelectedId] = useState(uniqueCategory[0]);
+  const [selectedDomain, setSelectedDomain] = useState(uniqueDomain[0]);
+
+  console.log(uniqueCategory);
+
+  const handelClick = (item, index) => {
+    setSelectedId(item);
+    setSelectedDomain(uniqueDomain[index]);
+  };
 
   return (
     <>
@@ -12,14 +22,16 @@ const ProjectCategory = ({ data, category, domain, renderComponet }) => {
           {uniqueCategory.map((item, index) => (
             <div
               key={index}
-              onClick={() => setSelectedId(item)}
+              onClick={() => handelClick(item, index)}
               className="categoryOptionItem"
             >
               {item}
             </div>
           ))}
         </div>
-
+        <div className="categoryTitle">
+          <CostumeHeading data={selectedDomain} />
+        </div>
         <div className="categoryCardSection">
           {data
             .filter((item) => item[category] === selectedId)
---
+++ src/components/Heading/CostumeHeading.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const CostumeHeading = ({ data }) => {
+    return (
+        <>
+            <div className="titleContainer">
+                <span className="tagAbout tagAbout1">{`<h2>`}</span>
+                <h1 className="title">{data}</h1>
+                <span className="tagAbout tagAbout2">{`</h2>`}</span>
+            </div>
+        </>
+    );
+};
+
+export default CostumeHeading;
