d639e34e3a7c6b7b78d4f4c9a8a1db7817a22c69
youtanimstar <animdeep2019@gmail.com>
2024-04-07 18:39:47 +0530

    cards updated

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -35,7 +35,7 @@ const Card = ({ className = "", data, ...props }) => {
               
           </div>
         </div>
-        <div className="absolute top-0 -inset-full h-full w-1/2 z-5 block transform -skew-x-12 bg-gradient-to-r from-transparent to-white opacity-40 group-hover:animate-shine" />
+        
       </div>
     </>
   );
